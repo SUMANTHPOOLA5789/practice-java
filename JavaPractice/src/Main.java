12ef35cb891d868752423da473757c3968ac5579
SUMANTHPOOLA5789 <sumanthp.cris@gmail.com>
2024-06-13 20:01:20 +0530

    feat: add JavaPractice project

---
+++ JavaPractice/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        /*
+        primitives
+         */
+        byte age;
+        age = 21;
+        short salaryMonthly;
+        salaryMonthly = 32456;
+        int salaryAnnually;
+        salaryAnnually = 4800000;
+        long phoneNumber;
+        phoneNumber = 8555039464L;
+        float productCost;
+        productCost = 89.5f;
+        double companyAnnualIncome;
+        companyAnnualIncome = 23244546456.3;
+        char litter;
+        litter = 'A';
+        boolean condition;
+        condition = true;
+        
+    }
+}
