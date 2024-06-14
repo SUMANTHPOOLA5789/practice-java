b12c6f29f7b13eb7781e78b5655e9459d3bace0c
SUMANTHPOOLA5789 <sumanthp.cris@gmail.com>
2024-06-14 09:20:27 +0530

    feat: add com.trainingmug.java.primitives.java and main.java delete

---
+++ JavaPractice/src/com/trainingmug/java/Primitives.java
@@ -0,0 +1,35 @@
+package com.trainingmug.java;
+
+public class Primitives {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        /*
+        primitives
+         */
+        byte age;
+        age = 21;
+        System.out.println("Age : " + age);
+        short salaryMonthly;
+        salaryMonthly = 32456;
+        System.out.println("Salary Monthly : "+ salaryMonthly);
+        int salaryAnnually;
+        salaryAnnually = 4800000;
+        System.out.println("Salary Annually : "+ salaryAnnually);
+        long phoneNumber;
+        phoneNumber = 8555039464L;
+        System.out.println("Phone Number : "+ phoneNumber);
+        float productCost;
+        productCost = 89.5f;
+        System.out.println("Product Cost : "+ productCost);
+        double companyAnnualIncome;
+        companyAnnualIncome = 23244546456.3;
+        System.out.println("Company Annual Income : "+ companyAnnualIncome);
+        char letter;
+        letter = 'A';
+        System.out.println("Letter : "+ letter);
+        boolean condition;
+        condition = true;
+        System.out.println("Condition : "+ condition);
+    }
+}
